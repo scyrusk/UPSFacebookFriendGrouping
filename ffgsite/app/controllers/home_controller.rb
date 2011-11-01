6d9ed9355bdd5a211ec6b08d281d19f9f969be3a
Sauvik Das <scyrus89@gmail.com>
2011-10-31 21:30:47 -0400

    Progress Report improvements and updated web application

--- ffgsite/app/controllers/home_controller.rb
+++ ffgsite/app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'twilio-ruby'
+require 'Digest'
 
 class HomeController < ApplicationController
   def index
@@ -7,8 +8,55 @@ class HomeController < ApplicationController
     auth_token = '4e443ae95126ba36969b4ca1208d5453'
     @client = Twilio::REST::Client.new account_sid, auth_token
     @messages = @client.account.sms.messages.list
+    Rails::logger.debug "Entering home controller index..."
     @messages.each do |message|
-      puts message.inspect
+      # create each partial post here for all posts not already created
+      # also, create a user for all "From" numbers not already assigned to a user
+      Rails::logger.debug message.body
+      user = User.find(:first, :conditions => ["phone_number = ?",message.from])
+      if (user == nil)
+        Rais::logger.debug 'Creating new user...'
+        user = User.new do |u|
+          u.phone_number = message.from
+          u.link = Digest::MD5.hexdigest(message.from)
+          u.save
+        end
+        Rails::logger.debug 'Done...'
+      end
+      
+      post = Post.find(:first, :conditions => ["sms_body = ?", message.body])
+      if (post == nil)
+        Rais::logger.debug 'Creating new post...'
+        post = Post.new do |p|
+          p.sms_date = message.date_sent
+          p.sms_body = message.body
+          p.user = user
+          p.save
+        end
+        Rails::logger.debug 'Done...'
+      end
     end
+
+    @users = User.find(:all)
+    if (@users == nil)
+      Rails::logger.debug '@users is nil for some reason'
+    else
+      Rails::logger.debug '@users isn\'t nil in the controller'
+    end
+    @posts = Post.find(:all)
+    if (@posts == nil)
+      Rails::logger.debug '@posts is nil for some reason'
+    else
+      Rails::logger.debug '@posts isn\'t nil in the controller'
+    end
+
+    userID = params[:id]
+    if userID == nil
+      Rails::logger.debug '@userID param is nil'
+    else
+      Rails::logger.debug '@userID param is ' << userID
+      @user = User.find_by_link(params[:id])
+    end
+
   end
 end
--- ffgsite/app/models/post.rb
+++ ffgsite/app/models/post.rb
@@ -1,16 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  def self.p1_prompt = 'Why did you decide not to post this to Facebook?' end
-  def self.p2_prompt = 'Were there any people you would especially not want to see this post?' end
-  def self.p3_prompt = 'Were there any people you would especially want to see this post?' end
-  def self.p4_prompt = 'Where were you when you decided not to post this to Facebook?' end
-  def self.p5_prompt = 'What were you doing when you decided not to post this to Facebook?' end
+  $p1_prompt = 'Why did you decide not to post this to Facebook?'
+  $p2_prompt = 'Were there any people you would especially not want to see this post?'
+  $p3_prompt = 'Were there any people you would especially want to see this post?'
+  $p4_prompt = 'Where were you when you decided not to post this to Facebook?'
+  $p5_prompt = 'What were you doing when you decided not to post this to Facebook?'
   
-  def self.np1_prompt = 'What type of post was it? (e.g. direct message, wall post, picture)' end
-  def self.np1_options = ['Direct Message','Wall Post','Picture','Video','Note'] end
-  def self.np2_prompt = 'Who was the intended audience for the post?' end
-  def self.np3_prompt = 'Why did you post it?' end
+  $np1_prompt = 'What type of post was it? (e.g. direct message, wall post, picture)'
+  $np1_options = ['Direct Message','Wall Post','Picture','Video','Note']
+  $np2_prompt = 'Who was the intended audience for the post?'
+  $np3_prompt = 'Why did you post it?'
   
-  def self.nn1_prompt = 'Why didn\'t you post anything to Facebook today?' end
-  def self.nn2_prompt = 'Why didn\'t you think about posting anything to Facebook today?' end
+  $nn1_prompt = 'Why didn\'t you post anything to Facebook today?'
+  $nn2_prompt = 'Why didn\'t you think about posting anything to Facebook today?'
 end
