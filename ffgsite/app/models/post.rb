1d1e13d628f5705b0adbd556c95b686a3d5217a5
Sauvik Das <scyrus89@gmail.com>
2011-11-02 14:10:24 -0400

    Newest version of the web application

--- ffgsite/app/models/post.rb
+++ ffgsite/app/models/post.rb
@@ -1,16 +1,29 @@
 class Post < ActiveRecord::Base
+  attr_accessible :sms_body,:p1,:p2,:p3,:p4,:p5,:np1,:np2,:np3,:nn1,:nn2
   belongs_to :user
-  $p1_prompt = 'Why did you decide not to post this to Facebook?'
-  $p2_prompt = 'Were there any people you would especially not want to see this post?'
-  $p3_prompt = 'Were there any people you would especially want to see this post?'
-  $p4_prompt = 'Where were you when you decided not to post this to Facebook?'
-  $p5_prompt = 'What were you doing when you decided not to post this to Facebook?'
   
-  $np1_prompt = 'What type of post was it? (e.g. direct message, wall post, picture)'
-  $np1_options = ['Direct Message','Wall Post','Picture','Video','Note']
-  $np2_prompt = 'Who was the intended audience for the post?'
-  $np3_prompt = 'Why did you post it?'
+  def update_completed
+    if (self.kind.casecmp("p") == 0 && meaningful_p ) || (self.kind.casecmp("np") && self.meaningful_np) || (self.kind.casecmp("nn") && self.meaningful_nn)
+      self.completed = true
+    else
+      self.completed = false
+    end
+    self.save
+  end
+
+  def meaningful( val )
+    val != nil && val.casecmp("") != 0
+  end
   
-  $nn1_prompt = 'Why didn\'t you post anything to Facebook today?'
-  $nn2_prompt = 'Why didn\'t you think about posting anything to Facebook today?'
+  def meaningful_p
+    (meaningful(self.p1) && meaningful(self.p2) && meaningful(self.p3) && meaningful(self.p4) && meaningful(self.p5))
+  end
+
+  def meaningful_np
+    (meaningful(self.np1) && meaningful(self.np2) && meaningful(self.np3))
+  end
+
+  def meaningful_nn
+    (meaningful(self.nn1) && meaningful(self.nn2))
+  end
 end
