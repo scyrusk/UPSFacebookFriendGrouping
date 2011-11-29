976f5320991d99104b83d40edee08375196f258f
scyrusk <sauvik@cmu.edu>
2011-11-28 21:52:19 -0500

    Changed time zone of newly created posts to east coast in the forgot posts control

--- ffgsite/app/controllers/posts_controller.rb
+++ ffgsite/app/controllers/posts_controller.rb
@@ -9,7 +9,10 @@ class PostsController < ApplicationController
       else
         @post = Post.new do |p|
           dateSplit = params[:date].split("/")
-          p.sms_date = DateTime.new(dateSplit[0].to_i,dateSplit[1].to_i,dateSplit[2].to_i).to_s
+          Rails::logger.debug params[:date]
+          dateSplit.each do |split| Rails::logger.debug '' + split end
+          p.sms_date = DateTime.new(dateSplit[0].to_i,dateSplit[1].to_i,dateSplit[2].to_i,4,0,0).in_time_zone("Eastern Time (US & Canada)")
+          Rails::logger.debug '' + p.sms_date.to_s
           p.post_date = p.sms_date
           p.sms_body = "[Blank]"
           p.kind = @type
